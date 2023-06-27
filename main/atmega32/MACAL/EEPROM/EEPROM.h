d3c1e9928ff21bafde33bd8842d9ed3fc6718351
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-06-27 18:09:00 +0300

    Create EEPROM.h

---
+++ main/atmega32/MACAL/EEPROM/EEPROM.h
@@ -0,0 +1,9 @@
+#ifndef EEPROM_DRIVER_H_
+#define EEPROM_DRIVER_H_
+
+void EEPROM_WRITE(unsigned short address,unsigned char data);
+/////////////////////////////////////////////////////////////////////////////////
+unsigned char EEPROM_READ(unsigned short address);
+
+
+#endif 
