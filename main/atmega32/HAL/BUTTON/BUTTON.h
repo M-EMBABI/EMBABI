fc5625a382d5c792f69e3375018c54ac848a598a
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-06-28 14:33:52 +0300

    Add files via upload

---
+++ main/atmega32/HAL/BUTTON/BUTTON.h
@@ -0,0 +1,18 @@
+/*
+ * BUTTON.h
+ *
+ * Created: 2/15/2023 1:40:01 AM
+ *  Author: MOHAMMED
+ */ 
+
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+void BUTTON_INIT(unsigned char port,unsigned char pin);
+////////////////////////////////////////////////////////////////
+unsigned char BUTTON_READ(unsigned char port,unsigned char pin);
+
+
+
+#endif /* BUTTON_H_ */
