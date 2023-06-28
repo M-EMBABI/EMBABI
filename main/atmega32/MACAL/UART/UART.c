07ebe12590913604a960b6e5c0bea664fd948f48
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-06-28 14:09:14 +0300

    Create UART.c

---
+++ main/atmega32/MACAL/UART/UART.c
@@ -0,0 +1,38 @@
+#define F_CPU 8000000UL
+#include "DIOFINAL.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include "STD_MACROS.h"
+////////////////////////////////////
+void UART_INIT(unsigned long baud)
+{
+	unsigned short UBRR;
+	UBRR=(F_CPU/(16*baud))-1 ;
+	UBRRH =(unsigned char)(UBRR>>8);
+	UBRRL =(unsigned char)(UBRR);
+	
+	SET_BET(UCSRB,TXEN);
+	SET_BET(UCSRB,RXEN);
+	
+	UCSRC=(1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);
+}
+void UART_SEND(char data)
+{
+	while(READ_BIT(UCSRA,UDRE)==0);
+	UDR=data;
+}
+unsigned char UART_RECEIVE(void)
+{
+	while(READ_BIT(UCSRA,RXC)==0);
+	
+	return UDR;
+}
+void UART_SEND_string(char *ptr)
+{
+	while (*ptr!=0)
+	{
+		UART_SEND(*ptr);
+		ptr++;
+		_delay_ms(100);
+	}
+}
