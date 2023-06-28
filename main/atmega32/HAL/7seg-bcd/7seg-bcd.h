74f97a1b762cf186d38f98d4066265b21c624025
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-06-28 14:40:48 +0300

    Create 7seg-bcd.h

---
+++ main/atmega32/HAL/7seg-bcd/7seg-bcd.h
@@ -0,0 +1,10 @@
+#ifndef SEV_SEG_BCD_H_
+#define SEV_SEG_BCD_H_
+void SEV_SEG_INIT(unsigned char port);
+//////////////////////////////////////////////////////////////////////////////////////
+void write_BCD(unsigned char port,unsigned char val);
+
+
+
+
+#endif /* SEV_SEG_BCD_H_ */
