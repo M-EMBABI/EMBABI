ccd03ba8ae67852532f98a5d70f7bbe2d070cc20
mohamed embabi <137911793+M-EMBABI@users.noreply.github.com>
2023-06-28 14:29:20 +0300

    Create 7seg-comman-cathod.h

---
+++ main/atmega32/HAL/7seg/7seg-comman-cathod.h
@@ -0,0 +1,10 @@
+#ifndef SEV_SEGMANT_C_PORT_H_
+#define SEV_SEGMANT_C_PORT_H_
+void SEV_SEG_INIT_PORT(unsigned char port);
+///////////////////////////////////////////////////////////
+void SEV_SEG_WRITE_PORT(unsigned char port,unsigned char number);
+
+
+
+
+#endif /* SEV_SEGMANT_C_PORT_H_ */
